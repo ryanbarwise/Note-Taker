11366cf018dd3ad38d94679450485bbe045afc28
Ryan Barwise <ryanbarwise@gmail.com>
2020-06-12 14:16:50 -0400

    working api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,44 @@
 var path = require("path");
 var data = require('../db/db.json');
+var fs = require('fs');
+
 
 module.exports = function(app) {
   // retrieve notes
   app.get("/api/notes", function(req, res) {
-     res.json(data);
-    
+     res.json(data)
   });
 
   // add note
   app.post("/api/notes", function(req, res) {
+    var id = data.length + 1;
+    var newNote = req.body;
+    newNote.id = id;
     data.push(req.body);
-    //res.json({data});
-   //res.sendFile(path.join(__dirname, "../public/notes.html"));
-   res.json(data);
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    console.log('sending new notes')
+    res.json(data); 
   });
+
+  //app.delete("/api/notes/:title")
 
   // delete note
   app.delete("/api/notes/:id", function(req, res) {
-    data.forEach((item, i) => {
-      req.params.id = i + 1;
-      console.log('Note ID: ', req.params.id)
-      // console.log('Note ID: ', req.params.id)
-     res.json(data)
-});
+  data = data.filter(item => {
+      // return every note in the array
+      // that does NOT have the same id
+      return item.id !== parseInt(req.params.id) 
+    });
+    
+    console.log(data.length)
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(data), function (err) {
+      if (err) throw err;
+      console.log('Deleted!');
+    });
+    res.json(data);
+
 })
 }
